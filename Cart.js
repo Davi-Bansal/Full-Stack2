2c75009e6259cccbf0c0185a5ab8bc2071df7d9f
Davi-Bansal <davybansal714@gmail.com>
2025-10-30 21:00:48 +0530

    Create Cart.js

---
+++ Cart.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const item = state.items.find((i) => i.id === action.payload.id);
+      if (item) {
+        item.qty += 1;
+      } else {
+        state.items.push({ ...action.payload, qty: 1 });
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter((i) => i.id !== action.payload);
+    },
+    incrementQty: (state, action) => {
+      const item = state.items.find((i) => i.id === action.payload);
+      if (item) item.qty += 1;
+    },
+    decrementQty: (state, action) => {
+      const item = state.items.find((i) => i.id === action.payload);
+      if (item && item.qty > 1) item.qty -= 1;
+    }
+  }
+});
+
+export const { addToCart, removeFromCart, incrementQty, decrementQty } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
